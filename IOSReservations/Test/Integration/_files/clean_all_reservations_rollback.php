2e7bef733be427746617344334803e82897d3cc1
Hans Nieser <hans@reachdigital.nl>
2018-09-28 15:19:41 +0200

    Remove clean reservation fixtures
    Moved to reach-digital/magento2-inventory-source-reservations

--- IOSReservations/Test/Integration/_files/clean_all_reservations_rollback.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-/** @var \Magento\Framework\App\ResourceConnection $resource */
-$resource   = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(\Magento\Framework\App\ResourceConnection::class);
-
-$resource->getConnection()->truncateTable($resource->getTableName('inventory_reservation'));
-$resource->getConnection()->truncateTable($resource->getTableName('inventory_source_reservation'));
