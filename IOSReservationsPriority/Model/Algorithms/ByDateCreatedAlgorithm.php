2abe5dc5581a4ea7ecbb3d8e329b1b38379f8b6a
Hans Nieser <hans@reachdigital.nl>
2018-11-01 11:05:25 +0100

    Implement skipping of fully-assigned orders
    Partially assigned orders aren’t support yet, but no further changes should be needed here if this becomes supported in the future.

--- IOSReservationsPriority/Model/Algorithms/ByDateCreatedAlgorithm.php
+++ IOSReservationsPriority/Model/Algorithms/ByDateCreatedAlgorithm.php
@@ -10,6 +10,7 @@ namespace ReachDigital\IOSReservationsPriority\Model\Algorithms;
 
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Api\SortOrderBuilder;
+use Magento\Framework\App\ResourceConnection;
 use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\Data\OrderSearchResultInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
@@ -34,22 +35,47 @@ class ByDateCreatedAlgorithm implements OrderSelectionInterface
      */
     private $sortOrderBuilder;
 
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
     public function __construct(
         OrderRepositoryInterface $orderRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder,
-        SortOrderBuilder $sortOrderBuilder
+        SortOrderBuilder $sortOrderBuilder,
+        ResourceConnection $resourceConnection
     ) {
         $this->orderRepository = $orderRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->sortOrderBuilder = $sortOrderBuilder;
+        $this->resourceConnection = $resourceConnection;
     }
 
     /**
+     * Get orders with one or more order items not yet assigned to source.
+     *
      * @inheritdoc
      */
     public function execute(?int $limit): OrderSearchResultInterface
     {
+        $conn = $this->resourceConnection->getConnection();
+        $resTable = $this->resourceConnection->getTableName('inventory_source_reservation');
+        $orderTable = $this->resourceConnection->getTableName('sales_order');
+        $orderItemTable = $this->resourceConnection->getTableName('sales_order_item');
+
+        // Fetch IDs of all orders being processed, which have at least one order_item without a source reservation
+        $select = $conn->select()
+            ->distinct(true)
+            ->from(     [ 'o'  => $orderTable     ], OrderInterface::ENTITY_ID)
+            ->joinInner([ 'oi' => $orderItemTable ], 'oi.order_id = o.entity_id', [])
+            ->joinLeft( [ 'r'  => $resTable       ], "r.metadata like concat('%order_item:', oi.item_id, '%')", [])
+            ->where(sprintf("r.reservation_id is null AND o.%s = '%s'", OrderInterface::STATE, Order::STATE_PROCESSING));
+
+        $orderIds = $conn->fetchCol($select);
+
         $this->searchCriteriaBuilder->addFilter(OrderInterface::STATE, Order::STATE_PROCESSING);
+        $this->searchCriteriaBuilder->addFilter(OrderInterface::ENTITY_ID, $orderIds, 'in');
 
         $sort = $this->sortOrderBuilder
             ->setField(OrderInterface::CREATED_AT)
@@ -57,7 +83,6 @@ class ByDateCreatedAlgorithm implements OrderSelectionInterface
             ->create();
         $this->searchCriteriaBuilder->addSortOrder($sort);
 
-        // @todo: only fetch non-assigned orders
         if ($limit) {
             $this->searchCriteriaBuilder->setPageSize($limit);
         }
