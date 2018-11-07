ecf67632d59ee979435d0fdcf21b0a841d276f66
Hans Nieser <hans@reachdigital.nl>
2018-11-07 16:50:04 +0100

    Add preferences for ReturnToStockInvoice, ReturnToStockOrder

---
+++ IOSReservations/Plugin/MagentoSalesInventory/AlwaysAutoReturnToStockInvoice.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace ReachDigital\IOSReservations\Plugin\MagentoSalesInventory;
+
+use Magento\SalesInventory\Model\Plugin\Order\ReturnToStockInvoice;
+
+class AlwaysAutoReturnToStockInvoice extends ReturnToStockInvoice
+{
+    /**
+     * @var \Magento\SalesInventory\Model\Order\ReturnProcessor
+     */
+    private $returnProcessor;
+
+    /**
+     * @var \Magento\Sales\Api\CreditmemoRepositoryInterface
+     */
+    private $creditmemoRepository;
+
+    /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var \Magento\Sales\Api\InvoiceRepositoryInterface
+     */
+    private $invoiceRepository;
+
+    /**
+     * @var \Magento\CatalogInventory\Api\StockConfigurationInterface
+     */
+    private $stockConfiguration;
+
+    /**
+     * ReturnToStockInvoice constructor.
+     * @param \Magento\SalesInventory\Model\Order\ReturnProcessor $returnProcessor
+     * @param \Magento\Sales\Api\CreditmemoRepositoryInterface $creditmemoRepository
+     * @param \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+     * @param \Magento\Sales\Api\InvoiceRepositoryInterface $invoiceRepository
+     * @param \Magento\CatalogInventory\Api\StockConfigurationInterface $stockConfiguration
+     */
+    public function __construct(
+        \Magento\SalesInventory\Model\Order\ReturnProcessor $returnProcessor,
+        \Magento\Sales\Api\CreditmemoRepositoryInterface $creditmemoRepository,
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        \Magento\Sales\Api\InvoiceRepositoryInterface $invoiceRepository,
+        \Magento\CatalogInventory\Api\StockConfigurationInterface $stockConfiguration
+    ) {
+        parent::__construct($returnProcessor, $creditmemoRepository, $orderRepository, $invoiceRepository, $stockConfiguration);
+        $this->returnProcessor = $returnProcessor;
+        $this->creditmemoRepository = $creditmemoRepository;
+        $this->orderRepository = $orderRepository;
+        $this->invoiceRepository = $invoiceRepository;
+        $this->stockConfiguration = $stockConfiguration;
+    }
+
+    /**
+     * @param \Magento\Sales\Api\RefundInvoiceInterface $refundService
+     * @param int $resultEntityId
+     * @param int $invoiceId
+     * @param \Magento\Sales\Api\Data\CreditmemoItemCreationInterface[] $items
+     * @param bool|null $isOnline
+     * @param bool|null $notify
+     * @param bool|null $appendComment
+     * @param \Magento\Sales\Api\Data\CreditmemoCommentCreationInterface|null $comment
+     * @param \Magento\Sales\Api\Data\CreditmemoCreationArgumentsInterface|null $arguments
+     * @return int
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterExecute(
+        \Magento\Sales\Api\RefundInvoiceInterface $refundService,
+        $resultEntityId,
+        $invoiceId,
+        array $items = [],
+        $isOnline = false,
+        $notify = false,
+        $appendComment = false,
+        \Magento\Sales\Api\Data\CreditmemoCommentCreationInterface $comment = null,
+        \Magento\Sales\Api\Data\CreditmemoCreationArgumentsInterface $arguments = null
+    ) {
+        $invoice = $this->invoiceRepository->get($invoiceId);
+        $order = $this->orderRepository->get($invoice->getOrderId());
+
+        $returnToStockItems = [];
+        if ($arguments !== null
+            && $arguments->getExtensionAttributes() !== null
+            && $arguments->getExtensionAttributes()->getReturnToStockItems() !== null
+        ) {
+            $returnToStockItems = $arguments->getExtensionAttributes()->getReturnToStockItems();
+        }
+        $isAutoReturn = $this->stockConfiguration->isAutoReturnEnabled();
+        if ($isAutoReturn || !empty($returnToStockItems)) {
+            $creditmemo = $this->creditmemoRepository->get($resultEntityId);
+            $this->returnProcessor->execute($creditmemo, $order, $returnToStockItems, $isAutoReturn);
+        }
+        return $resultEntityId;
+    }
+}
