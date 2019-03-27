103c57357d1eb01ebff68af7e9b483c52ea49e82
Hans Nieser <hans@reachdigital.nl>
2019-03-27 17:38:01 +0100

    MD-41 Extend order items with associated source reservations

---
+++ IOSReservations/Plugin/MagentoSales/LoadSourceReservationsWithOrderItem.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace ReachDigital\IOSReservations\Plugin\MagentoSales;
+
+use Magento\Sales\Api\Data\OrderItemInterface;
+use Magento\Sales\Api\Data\OrderItemSearchResultInterface;
+use Magento\Sales\Api\OrderItemRepositoryInterface;
+use ReachDigital\ISReservations\Model\MetaData\EncodeMetaData;
+use ReachDigital\ISReservations\Model\ResourceModel\GetReservationsByMetadata;
+
+class LoadSourceReservationsWithOrderItem
+{
+
+    /**
+     * @var GetReservationsByMetadata
+     */
+    private $getReservationsByMetadata;
+
+    /**
+     * @var EncodeMetaData
+     */
+    private $encodeMetaData;
+
+    public function __construct(
+        GetReservationsByMetadata $getReservationsByMetadata,
+        EncodeMetaData $encodeMetaData
+    )
+    {
+        $this->getReservationsByMetadata = $getReservationsByMetadata;
+        $this->encodeMetaData = $encodeMetaData;
+    }
+
+    public function afterGet(
+        /** @noinspection PhpUnusedParameterInspection */
+        OrderItemRepositoryInterface $subject,
+        OrderItemInterface $orderItem
+    ) : OrderItemInterface
+    {
+        $extensionAttributes = $orderItem->getExtensionAttributes();
+        $orderId = $orderItem->getOrderId();
+        $orderItemId = $orderItem->getItemId();
+        $reservations = $this->getReservationsByMetadata->execute(
+            $this->encodeMetaData->execute([ 'order' => $orderId, 'order_item' => $orderItemId])
+        );
+        /** @noinspection NullPointerExceptionInspection - should always be set */
+        $extensionAttributes->setSourceReservations($reservations);
+        return $orderItem;
+    }
+
+    public function afterGetList(
+        /** @noinspection PhpUnusedParameterInspection */
+        OrderItemRepositoryInterface $subject,
+        OrderItemSearchResultInterface $orderItemSearchResult
+    ) : OrderItemSearchResultInterface
+    {
+        foreach ($orderItemSearchResult->getItems() as $item) {
+            $extensionAttributes = $item->getExtensionAttributes();
+            $orderId = $item->getOrderId();
+            $orderItemId = $item->getItemId();
+            $reservations = $this->getReservationsByMetadata->execute(
+                $this->encodeMetaData->execute([ 'order' => $orderId, 'order_item' => $orderItemId])
+            );
+            /** @noinspection NullPointerExceptionInspection - should always be set */
+            $extensionAttributes->setSourceReservations($reservations);
+        }
+        return $orderItemSearchResult;
+    }
+}
