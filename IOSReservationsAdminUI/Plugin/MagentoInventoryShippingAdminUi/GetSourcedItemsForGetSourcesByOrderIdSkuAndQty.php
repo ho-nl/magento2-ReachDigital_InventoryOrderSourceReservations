acc478fe51fc0193a840645da51f0f0639bfc6ef
Paul Hachmang <paul@reachdigital.nl>
2020-06-19 12:31:33 +0200

    feat(ZV-10): allow creation of shipment based on already made reservations
    docs: this requires a new module to be installed: `bin/magento module:enable ReachDigital_IOSReservationsAdminUi

---
+++ IOSReservationsAdminUI/Plugin/MagentoInventoryShippingAdminUi/GetSourcedItemsForGetSourcesByOrderIdSkuAndQty.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace ReachDigital\IOSReservationsAdminUi\Plugin\MagentoInventoryShippingAdminUi;
+
+use Closure;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\InventoryApi\Api\SourceRepositoryInterface;
+use Magento\InventoryShippingAdminUi\Ui\DataProvider\GetSourcesByOrderIdSkuAndQty;
+use ReachDigital\IOSReservations\Model\GetOrderSourceReservations;
+use ReachDigital\ISReservations\Model\MetaData\EncodeMetaData;
+use ReachDigital\ISReservations\Model\ResourceModel\GetReservationsByMetadata;
+
+class GetSourcedItemsForGetSourcesByOrderIdSkuAndQty
+{
+    /**
+     * @var GetReservationsByMetadata
+     */
+    private $getReservationsByMetadata;
+
+    /**
+     * @var SourceRepositoryInterface
+     */
+    private $sourceRepository;
+
+    /**
+     * @var string[]
+     */
+    private $sources;
+    /**
+     * @var EncodeMetaData
+     */
+    private $encodeMetaData;
+
+    public function __construct(
+        GetReservationsByMetadata $getReservationsByMetadata,
+        EncodeMetaData $encodeMetaData,
+        SourceRepositoryInterface $sourceRepository
+    ) {
+        $this->getReservationsByMetadata = $getReservationsByMetadata;
+        $this->sourceRepository = $sourceRepository;
+        $this->encodeMetaData = $encodeMetaData;
+    }
+
+    /**
+     * We're not calling the original method because we don't want to allow for any SSA to run at this point.
+     *
+     * @param GetSourcesByOrderIdSkuAndQty $subject
+     * @param Closure $proceed
+     * @param int $orderId
+     * @param string $sku
+     * @param float $qty
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function aroundExecute(
+        GetSourcesByOrderIdSkuAndQty $subject,
+        Closure $proceed,
+        int $orderId,
+        string $sku,
+        float $qty
+    ) {
+        $reservations = $this->getReservationsByMetadata->execute(
+            $this->encodeMetaData->execute(['order' => $orderId])
+        );
+
+        $result = [];
+
+        foreach ($reservations as $sourceReservation) {
+            if ($sourceReservation->getSku() === $sku) {
+                $sourceCode = $sourceReservation->getSourceCode();
+
+                $qty += $sourceReservation->getQuantity();
+
+                if (!isset($result[$sourceCode])) {
+                    $result[$sourceCode] = [
+                        'sourceName' => $this->getSourceName($sourceCode),
+                        'sourceCode' => $sourceCode,
+                        'qtyAvailable' => $sourceReservation->getQuantity() * -1,
+                        'qtyToDeduct' => $sourceReservation->getQuantity() * -1,
+                    ];
+                } else {
+                    $result[$sourceCode]['qtyAvailable'] += $sourceReservation->getQuantity() * -1;
+                    $result[$sourceCode]['qtyToDeduct'] += $sourceReservation->getQuantity() * -1;
+                }
+            }
+        }
+
+        foreach ($result as $key => $item) {
+            if ($this->isLessThanAlmostZero($item['qtyToDeduct'])) {
+                unset($result[$key]);
+            }
+        }
+
+        return array_values($result);
+    }
+
+    /**
+     * Get source name by code
+     *
+     * @param string $sourceCode
+     * @return mixed
+     * @throws NoSuchEntityException
+     */
+    private function getSourceName(string $sourceCode): string
+    {
+        if (!isset($this->sources[$sourceCode])) {
+            $this->sources[$sourceCode] = $this->sourceRepository->get($sourceCode)->getName();
+        }
+
+        return $this->sources[$sourceCode];
+    }
+
+    /**
+     * Compare float number with some epsilon
+     *
+     * @param float $floatNumber
+     *
+     * @return bool
+     */
+    private function isLessThanAlmostZero(float $floatNumber): bool
+    {
+        return $floatNumber < 0.0000001;
+    }
+}
