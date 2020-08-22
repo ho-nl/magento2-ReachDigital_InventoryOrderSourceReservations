ae116487a795974d36adf51a9f4e167598c3f0a5
Paul Hachmang <paul@reachdigital.nl>
2020-08-22 11:44:29 +0200

    refactor: remove useless constructor on exceptions

--- IOSReservationsApi/Exception/CouldNotCreateSourceSelectionRequestFromOrder.php
+++ IOSReservationsApi/Exception/CouldNotCreateSourceSelectionRequestFromOrder.php
@@ -12,11 +12,6 @@ use Magento\Framework\Phrase;
 
 class CouldNotCreateSourceSelectionRequestFromOrder extends LocalizedException
 {
-    function __construct(Phrase $phrase, Exception $cause = null, $code = 0)
-    {
-        parent::__construct($phrase, $cause, $code);
-    }
-
     public static function create(int $orderId)
     {
         return new self(__('No source items available to assign to order %s', $orderId));
